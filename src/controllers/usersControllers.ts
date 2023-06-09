32d8501bf52de0cf8c020278e9dc9cf0969d4348
Ignacio Joaquín Soler <ignaciojsoler@gmail.com>
2023-06-09 15:56:40 -0300

    Feature: create user controller

--- src/controllers/usersControllers.ts
+++ src/controllers/usersControllers.ts
@@ -3,11 +3,25 @@ import { PrismaClient, User } from "@prisma/client";
 import { v4 } from "uuid";
 
 const prisma = new PrismaClient();
-const uuid = v4();
 
+//Create
 export const createUser = async (req: Request, res: Response) => {
+  const uuid = v4();
   const newUser: User = req.body;
   try {
+    const userExists = await prisma.user.findUnique({
+      where: {
+        email: newUser.email,
+      },
+    });
+
+    if (userExists)
+      return res
+        .status(400)
+        .json({
+          msg: `El correo ${newUser.email} ya se encuentra registrado.`,
+        });
+
     const result = await prisma.user.create({
       data: {
         id: uuid,
@@ -18,6 +32,6 @@ export const createUser = async (req: Request, res: Response) => {
     });
     res.json(result);
   } catch (e) {
-    throw new Error("No se ha podido agregar el usuario correctamente");
+    res.status(500).json({msg: "No se ha podido agregar el usuario correctamente"});
   }
 };
